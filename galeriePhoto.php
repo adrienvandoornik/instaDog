b8f2a7f1af471d1afc83707d72a8258a2e2d2d7e
Adrien Van Doornik <adrien.vandoornik@gmail.com>
2019-01-29 10:20:31 +0100

    galerie photo

--- galeriePhoto.php
+++ galeriePhoto.php
@@ -8,6 +8,8 @@
   <link rel="stylesheet" href="galeriePhoto.css">
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO"
     crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=PT+Serif:700" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Work+Sans:900" rel="stylesheet">
   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
     crossorigin="anonymous"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
@@ -27,40 +29,144 @@ include 'header.php';
 <!-- 
 ////Galerie de Photo////////////////////////////////////////////////////////////////////////////////////////////////////////// -->
 
-<div class="container">
-  <div class="d-flex justify-content-center">
- <div class="card mt-5" style="width:900px">
-  <img class="card-img-top" src="https://proxy.duckduckgo.com/iu/?u=http%3A%2F%2Fkimsulli.files.wordpress.com%2F2011%2F06%2Fdog3.jpg&f=1" alt="Lights" style="width:100%" alt="Card image">
-  <div class="card-img-overlay">
-      <h4 class="card-title">John Doe</h4>
-      <a href="#" class="btn btn-success">Voir Profil</a>
-    </div>
-  </div>
- 
-</div>
-</div>
-</div>
+<div class="container-fluid">
+  <div class="row no-gutters">
+        <div class="col-md-4 col-lg-4">
+        <a href="#">
+            <div class="square" style="background-image: url('image/chien1.jpg')">
+                <div class="card-img-overlay">
+                    <h4 class="card-title" style="font-size:3vw;">John Doe</p>
+                </div>
+            </div>
+        </a>
+      </div>
 
-<div class="container">
-  <div class="d-flex justify-content-center">
- <div class="card mt-5" style="width:900px">
-  <img class="card-img-top" src="https://proxy.duckduckgo.com/iu/?u=https%3A%2F%2Fstatic.wamiz.fr%2Fimages%2Fadoption%2Flarge%2Fchien-flat-coated-retriever-adopter-107830.JPG&f=1" alt="Lights" style="width:100%" alt="Card image">
-  <div class="card-img-overlay">
-      <h4 class="card-title">Rocky</h4>
-      <a href="#" class="btn btn-success">Voir Profil</a>
-    </div>
+      <div class="col-md-4 col-lg-4">
+        <a href="#">
+            <div class="square" style="background-image: url('image/chien2.jpg')">
+                <div class="card-img-overlay">
+                    <h4 class="card-title" style="font-size:3vw;">John Doe</p>
+                </div>
+            </div>
+        </a>
+      </div>
+
+      <div class="col-md-4 col-lg-4">
+        <a href="#">
+            <div class="square" style="background-image: url('image/chien3.jpg')">
+                <div class="card-img-overlay">
+                    <h4 class="card-title" style="font-size:3vw;">John Doe</p>
+                </div>
+            </div>
+        </a>
+      </div>
   </div>
- 
-</div>
-</div>
-</div>
+
+  <div class="row no-gutters">
+        <div class="col-md-4 col-lg-4">
+        <a href="#">
+            <div class="square" style="background-image: url('image/chien4.jpg')">
+                <div class="card-img-overlay">
+                    <h4 class="card-title" style="font-size:3vw;">John Doe</p>
+                </div>
+            </div>
+        </a>
+      </div>
+
+      <div class="col-md-4 col-lg-4">
+        <a href="#">
+            <div class="square" style="background-image: url('image/chien5.jpg')">
+                <div class="card-img-overlay">
+                    <h4 class="card-title" style="font-size:3vw;">John Doe</p>
+                </div>
+            </div>
+        </a>
+      </div>
+
+      <div class="col-md-4 col-lg-4">
+        <a href="#">
+            <div class="square" style="background-image: url('image/chien1.png')">
+                <div class="card-img-overlay">
+                    <h4 class="card-title" style="font-size:3vw;">John Doe</p>
+                </div>
+            </div>
+        </a>
+      </div>
+  </div>
+
+  <div class="row no-gutters">
+        <div class="col-md-4 col-lg-4">
+        <a href="#">
+            <div class="square" style="background-image: url('image/chien2.png')">
+                <div class="card-img-overlay">
+                    <h4 class="card-title" style="font-size:3vw;">John Doe</p>
+                </div>
+            </div>
+        </a>
+      </div>
+
+      <div class="col-md-4 col-lg-4">
+        <a href="#">
+            <div class="square" style="background-image: url('image/chien6.jpg')">
+                <div class="card-img-overlay">
+                    <h4 class="card-title" style="font-size:3vw;">John Doe</p>
+                </div>
+            </div>
+        </a>
+      </div>
+
+      <div class="col-md-4 col-lg-4">
+        <a href="#">
+            <div class="square" style="background-image: url('image/chien4.png')">
+                <div class="card-img-overlay">
+                    <h4 class="card-title" style="font-size:3vw;">John Doe</p>
+                </div>
+            </div>
+        </a>
+      </div>
+  </div>
+
+  <div class="row no-gutters">
+        <div class="col-md-4 col-lg-4">
+        <a href="#">
+            <div class="square" style="background-image: url('image/chien7.jpg')">
+                <div class="card-img-overlay">
+                    <h4 class="card-title" style="font-size:3vw;">John Doe</p>
+                </div>
+            </div>
+        </a>
+      </div>
+
+      <div class="col-md-4 col-lg-4">
+        <a href="#">
+            <div class="square" style="background-image: url('image/chien2.jpg')">
+                <div class="card-img-overlay">
+                    <h4 class="card-title" style="font-size:3vw;">John Doe</p>
+                </div>
+            </div>
+        </a>
+      </div>
+
+      <div class="col-md-4 col-lg-4">
+        <a href="#">
+            <div class="square" style="background-image: url('image/chien4.jpg')">
+                <div class="card-img-overlay">
+                    <h4 class="card-title" style="font-size:3vw;">John Doe</p>
+                </div>
+            </div>
+        </a>
+      </div>
+  </div>
+
+</div>  
+
 
   
   <!-- /////Footer////////////////////////////////////////////////////////////////////////////////////////////////////////// -->
   <?php
-  include 'footer.php';
-  
-  ?>
+    include 'footer.php';
+
+    ?>
 
 
 
