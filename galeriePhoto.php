3580661497e9cef1fd0babdfc8074a7abe50c0f0
Adrien Van Doornik <adrien.vandoornik@gmail.com>
2019-01-29 12:58:23 +0100

    CSS

--- galeriePhoto.php
+++ galeriePhoto.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 
 <head>
   <title>InstaDog</title>
@@ -8,14 +8,14 @@
   <link rel="stylesheet" href="galeriePhoto.css">
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO"
     crossorigin="anonymous">
-    <link href="https://fonts.googleapis.com/css?family=PT+Serif:700" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Work+Sans:900" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Work+Sans:300,500,900" rel="stylesheet">
   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
     crossorigin="anonymous"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
     crossorigin="anonymous"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy"
     crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
 </head>
 
 <body>
--- header.php
+++ header.php
@@ -4,7 +4,7 @@
 <!-- ////HEADER////////////////////////////////////////////////////////////////////////////////////////////////////////// -->
 
 <nav class="navbar navbar-expand-lg navbar-light bg-dark ">
-    <a class="navbar-brand text-light" href="galeriePhoto.php">InstaDog</a>
+    <a class="navbar-brand text-light" href="galeriePhoto.php">INSTADOG</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#demo-navbar" aria-controls="demo-navbar"
       aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
