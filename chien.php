fb0ad21226b08bccdffa21b1778b81b51b083285
Adrien Van Doornik <adrien.vandoornik@gmail.com>
2019-01-30 12:00:32 +0100

    class chien

---
+++ chien.php
@@ -0,0 +1,21 @@
+<?php
+
+class Chien
+{
+    private $idChien;
+    private $surnomChien;
+    private $nomElevageChien;
+    private $ageChien;
+    private $sexeChien;
+    private $raceChien;
+    private $listeArticleChien;
+
+
+    public function__set ($name, $value) { }
+
+    public function getArticle(){return $this->article;}
+
+
+}
+
+?>
