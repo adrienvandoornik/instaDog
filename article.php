cfdca54213b88c46f6d3694e05d6dfba0d84e4d8
Adrien Van Doornik <adrien.vandoornik@gmail.com>
2019-02-08 17:02:55 +0100

    formulaire action commentaire

--- article.php
+++ article.php
@@ -58,7 +58,7 @@
     <div class="container">
         <div class="container mt-5">
             <h3><i class="fas fa-comments"></i> Commentaires</h3>
-            <div class="media border p-3 bg-white">
+            <div class="media border p-3 bg-white"> /
                 <img src="https://proxy.duckduckgo.com/iu/?u=https%3A%2F%2Fcdn3.iconfinder.com%2Fdata%2Ficons%2Fbusiness-round-flat-vol-1-1%2F36%2Fuser_account_profile_avatar_person_student_male-512.png&f=1"
                     alt="John Doe" class="mr-3 mt-3 rounded-circle" style="width:60px;">
                 <div class="media-body">
@@ -73,7 +73,7 @@
 ///Inserer Commentaires///////////////////////////////////////////////////////////////////////////////////////////////////////// -->
 
     <div class="container mt-3">
-        <form action="/action_page.php">
+        <form  action="formulaire_action_commentaire.php" method="post" class="needs-validation formPadding" novalidate>
             <div class="form-group">
                 <label for="commentaire"><i class="fas fa-comment-medical"></i> Comment:</label>
                 <textarea class="form-control" rows="5" id="commentaire" name="texte" placeholder="Entrer un commentaire" value="" required></textarea>
